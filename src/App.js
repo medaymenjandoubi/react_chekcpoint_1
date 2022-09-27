f32815b707812731b484a54a6d3e57c12705d41b
medaymenjandoubi <medaymenjandoubi4@gmail.com>
2022-09-27 08:08:29 +0100

    commit	deleted:    .gitignore commit	modified:   package-lock.json commit	modified:   package.json commit	modified:   src/App.css commit	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from 'react-bootstrap/Nav'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 function App() {
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav fill variant="tabs" defaultActiveKey="/home" >
+      <Nav.Item className='navvy'>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item className='navvy'>
+        <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+      </Nav.Item>
+      <Nav.Item className='navvy'>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item className='navvy'>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+
+
+
+    <Carousel className='caroussel'>
+      <Carousel.Item>
+        <img
+          className="test"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLOJLwmpADKp-0Mj_A5aqM6hs_W2hyt_D7Vg&usqp=CAU"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="test"
+          src="https://i.ytimg.com/vi/isa3fk_oSUQ/maxresdefault.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="test"
+          src='https://africabusinesscommunities.com/Images/Key%20Logos/GOMYCODE%20Tunisia.jpeg'
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    <Form className='formcss'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label className='text'>Email address</Form.Label>
+        <Form.Control className='placeholder' type="email" placeholder="Enter email" />
+        <Form.Text className="text">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label className='text'>Password</Form.Label>
+        <Form.Control className='placeholder' type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-33" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        log in
+      </Button>
+    </Form>
+
+
+    <CardGroup className='cardgroup'>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
     </div>
   );
 }
